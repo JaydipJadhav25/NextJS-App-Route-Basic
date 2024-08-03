15e9a0589f705384d5209edbc92709ebda4fc28c
jaydipjadhav <jaydipdjadhav>
2024-08-03 13:30:02 +0530

    user Router push and replace methods

--- pages/[name]/[user]/setting.js
+++ pages/[name]/[user]/setting.js
@@ -3,6 +3,13 @@ import { useRouter } from "next/router";
 const Index =()=>{
 const router= useRouter();
 console.log(router)
-return <h1>This Is Setting Page  : {router.query.name} In {router.query.user} MODE</h1>
+return (
+    <div>
+        <h1>This Is Setting Page  : {router.query.name} In {router.query.user} MODE</h1>
+        <button
+        onClick={e => router.push("/")}
+        >GO TO HOME</button>
+    </div>
+)
 }
 export default Index;
--- pages/users/[username].js
+++ pages/users/[username].js
@@ -3,7 +3,32 @@ import { useRouter } from "next/router";
 const DyanamicPage = () =>{
     const router = useRouter();
     console.log(router);
-        return <h2>This is {router.query.username} page</h2>
+        return (
+        <div>
+            <h2>This is {router.query.username} page</h2>
+            {/* case : 1 */}
+            <button onClick={e => router.push(`/${router.query.username}/public/setting`)}>Click On GO TO Setting Case:1</button>
+            {/* case : 2 */}
+            <br/>
+            <button
+            onClick={e=> router.push({
+            pathname : "/[name]/[user]/setting",
+            query : {
+                name: router.query.username,
+                 user : "public"
+                }
+            })}
+            >
+           GO TO Setting Page Case:2
+            </button>
+      <br/>
+            <button
+            onClick={e => router.replace("/")}
+            >
+                To go Home  Using replace
+            </button>
+        </div>
+        );
     
     }
     export default DyanamicPage;
